caef86b99f65a7ae042403a211e9d22c8736d249
Aaron Ramos <ramosab@hawaii.edu>
2025-04-14 22:14:12 -1000

    Finish initial landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 
 import HeroSec from '@/components/HeroSec';
 import LandingHome from '@/components/LandingHome';
+import { Navbar } from 'react-bootstrap'
 
 /** The Home page. */
 const Home = () => (
   <main>
     <HeroSec />
+    <Navbar className="pt-3 bg-manoa-green"/>
     <LandingHome />
   </main>
 );
--- src/components/HeroSec.tsx
+++ src/components/HeroSec.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Col, Container, Image, Row, Button, Card } from 'react-bootstrap';
+import { Col, Container, Image, Row, Button, Card, Navbar, Nav } from 'react-bootstrap';
 import text_logo from '../../public/assets/manoa-connect_logo-text.svg'
 import card_image from '../../public/img/hero-img.jpg';
 
@@ -21,9 +21,13 @@ const HeroSec = () => (
                 </Card>
             </Col>
             <Col className="justify-content-center align-middle text-center">
-                <Image src={text_logo.src} width="650px" alt="Manoa Connect" className="invert"/>
+                <Image src={text_logo.src} width="650px" alt="Manoa Connect" className="invert mt-5"/>
                 <h3 className='py-2 ps-4 text-light'>Connecting made easy.</h3>
-                <Button className="btn-success my-1 py-2 px-5">Get Started</Button>
+                <Button className="btn-success my-1 py-2 px-5" href="/auth/signup">Get Started</Button>
+                <Navbar className="justify-content-center align-middle text-center text-light">
+                    <Navbar.Text className="text-light me-2">Already have an account?</Navbar.Text>
+                    <Nav.Link className="hover-line" href="/auth/signin">Sign in</Nav.Link>
+                </Navbar>
             </Col>
             <Col s={1} />
         </Row>
