e2891b011893b0c073ef85744748cdd1723ca559
Codie Nakamura <codien@hawaii.edu>
2025-04-22 11:05:16 -1000

    not done

--- src/components/UserHome.tsx
+++ src/components/UserHome.tsx
@@ -4,6 +4,7 @@ import { loggedInProtectedPage } from '@/lib/page-protection';
 import authOptions from '@/lib/authOptions';
 import Link from 'next/link';
 import { PeopleFill } from 'react-bootstrap-icons';
+import { prisma } from '@/lib/prisma';
 // import { prisma } from '@/lib/prisma';
 
 /** Render a list of stuff for the logged in user. */
@@ -17,19 +18,17 @@ const UserHome = async () => {
     } | null,
   );
   
-  /**
   const email = (session && session.user && session.user.email) || '';
-  const profile = await prisma.profile.findMany({
+  const profile = await prisma.profile.findUnique({
     where: {
       email,
     },
   });
-  */
   // console.log(stuff);
   return (
     <main>
       <Container id="home" fluid className="py-4 px-3">
-        <h2 className='text-center mb-4'>User Home Page</h2>
+        <h2 className='text-center mb-4'>Welcome, {profile?.firstName}!</h2>
         <Row>
         <Col md={6} className='d-flex flex-column'>
           <Card className='mb-3 p-3 text-center'>
@@ -40,15 +39,15 @@ const UserHome = async () => {
               <CardTitle>You have XX new messages!</CardTitle>
             </Card>
           </Link>
-          <Button className="btn-success py-2 px-4 w-100" href="/connect">
+          <Button className="btn-success py-4 px-4 w-100" href="/connect">
             <PeopleFill /> Connect
           </Button>
         </Col>
         <Col md={6} className="d-flex flex-column">
-          <Card className='mb-3 p-3 text-center'>
+          <Card className='mb-3 mt-3 p-5 text-center'>
             <CardTitle>You have made XX total new friends!</CardTitle>
           </Card>
-          <Button className="btn-success py-2 px-4 w-100" href="/editProfile">
+          <Button className="btn-success py-4 px-4 w-100" href="/editProfile">
             <PeopleFill /> Edit Profile
           </Button>
         </Col>
