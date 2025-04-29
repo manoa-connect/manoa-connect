14f065bd3c88355539e2673ecaa7f5a7e558cf90
Masaki-Sk <skmmks320@gmail.com>
2025-04-28 14:58:37 -1000

    Update ChatItem.tsx

--- src/components/ChatItem.tsx
+++ src/components/ChatItem.tsx
@@ -3,15 +3,27 @@
 import { ListGroup } from 'react-bootstrap';
 import { Chat } from '@prisma/client';
 
-/* Renders a single row in the List Stuff table. See list/page.tsx.  Comment*/
-const ChatItem = ({ chat }: { chat: Chat }) => (
-  <ListGroup.Item>
-    <p className="fw-lighter">
-      {chat.owner}
-      &nbsp;&nbsp;
-      {chat.createdAt.toLocaleDateString('en-US')}</p>
-    <p>{chat.chat}</p>
-  </ListGroup.Item>
-);
+/* Renders a single row in the List Stuff table. See list/page.tsx. */
+const ChatItem = ({ chat, currentUserEmail }: { chat: Chat; currentUserEmail: string }) => {
+  const isOwnMessage = chat.owner === currentUserEmail;
+
+  return(
+    <ListGroup.Item
+      className={`${isOwnMessage ? 'text-end' : 'text-start'} border-0`}
+    >
+      <p className="fw-lighter">
+        {isOwnMessage ? '' : chat.owner}
+        &nbsp;
+        {new Date(chat.createdAt).toLocaleTimeString('en-US', {
+          month: '2-digit',
+          day: '2-digit',
+          hour: '2-digit',
+          minute: '2-digit'
+          })}
+      </p>
+      <p className="mb-0">{chat.chat}</p>
+    </ListGroup.Item>
+  );
+};
 
 export default ChatItem;
